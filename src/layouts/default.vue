804edde49a5460258944aca0b8879cee0e49010c
Peter Akaliro <pin4eva@hotmail.com>
2024-06-30 23:49:29 +0100

    added layouts

---
+++ src/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <header>
+        <nav>
+            <nuxt-link href="/">Home</nuxt-link>
+            <nuxt-link href="/about">About</nuxt-link>
+        </nav>
+    </header>
+    <slot />
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,7 @@
 <template>
-    <h1>Home page</h1>
+    <h1>Home page 1</h1>
 </template>
+
+<script setup lang="ts">
+definePageMeta({ layout: "default" })
+</script>
